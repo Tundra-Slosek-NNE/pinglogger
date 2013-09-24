4cf9e9a5d1f3b2748fa5c08ef78315ca71056c97
Tundra Slosek <tslosek@nnenews.com>
2013-09-24 09:54:17 -0400

    First pass of changing ping_logger to XML format

--- ping_logger.pl
+++ ping_logger.pl
@@ -19,6 +19,17 @@ subdirectory doesn't yet exist it will be created. Each report submitted is
 saved with the starttime within the report as the filename on the logging 
 host.
 
+The format of the data at the command line is XML that has been bzip2 
+compressed and then base64 encoded. The decoded and bzip2 decompressed XML 
+is what is stored in the data directory. 
+
+=head1 HISTORICAL FORMAT
+
+Verions 1 of the exchange data was used previously. It had no format version 
+number in the file. It was line oriented key=value with the raw multiline
+ping output packed into a single line by replacing the newlines with a 
+string of three tilde characters (~~~). 
+
 =head1 DEPENDANCIES
 
 ping_logger.pl requires the following modules to already be in the local 
@@ -32,58 +43,63 @@ Perl install:
 
 =item File::Spec
 
+=item XML::Twig
+
+=item Compress::Bzip2
+
 =back
 
 =cut
 
-
+use strict;
 use MIME::Base64;
 use Digest::MD5 qw(md5_hex);
 use File::Spec;
+use XML::Twig;
+use Compress::Bzip2;
 
-$rawreport = decode_base64($ARGV[0]);
+my $rawreport;
 
-$datadir = "/var/pinglogger/";
+$rawreport = memBunzip(decode_base64($ARGV[0]));
 
-foreach $line (split("\n", $rawreport)) {
-    $line =~ s/\s+\Z//;
-    if ($line =~ m/\=/) {
-	my $key;
-	my $value;
-	($key, $value) = split('=', $line, 2);
-	if (lc $key eq 'starttime') {
-	    unless ($value =~ m/\D/) {
-		$starttime = $value;
-	    }
-	}
-	elsif (lc $key eq 'description') {
-	    $description = $value;
-	}
-	
-    }
-    else {
-	# silently discard lines without an equal sign
-    }
+my $twig = XML::Twig->new();
+
+unless ($twig->safe_parse($rawreport)) {
+    die 'Unable to parse the report, aborting. Error from XML::Twig:' 
+        . "\n" . $@; 
 }
+
+
+my $datadir = "/var/pinglogger/";
+
+my $formatversion = $twig->root->first_child_text('FormatVersion');
+
+unless ($formatversion == 2) {
+    die "Don't know how to handle format version '" . $formatversion 
+        . "', aborting.\n";
+}   
+
+my $starttime = $twig->root->first_child_text('Starttime');
+my $description = $twig->root->first_child_text('Description');
 
 if ($starttime && $description) {
     $dirname = File::Spec->catdir($datadir, md5_hex($description));
     unless (-d $dirname) {
-	mkdir $dirname;
-	unless (-d $dirname) {
-	    die "Unable to create directory $dirname, aborting.\n";
-	}
+    	mkdir $dirname;
+    	unless (-d $dirname) {
+    	    die "Unable to create directory $dirname, aborting.\n";
+    	}
     }
     $filename = File::Spec->catfile($dirname, $starttime);
     
     if (open(DATAFILE, '>' . $filename)) {
-	print DATAFILE $rawreport;
-	close(DATAFILE);
+    	print DATAFILE $rawreport;
+    	close(DATAFILE);
     }
     else {
-	print "Unable to open $filename for writing, aborting.\n";
+    	print "Unable to open $filename for writing, aborting.\n";
     }
 }
 else {
-    # missing parameters, throw away the report.
+    die "Required tags Starttime and Description not found, aborting.\n";
 }
