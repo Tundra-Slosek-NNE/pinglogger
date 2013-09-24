332752385a6a42f6b6282716cd83cc4bbfc6a053
Tundra Slosek <tslosek@nnenews.com>
2013-09-24 10:36:15 -0400

    Allow for configuration file for ping_logger.pl

--- ping_logger.pl
+++ ping_logger.pl
@@ -60,7 +60,97 @@ use Compress::Bzip2;
 
 my $rawreport;
 
-$rawreport = memBunzip(decode_base64($ARGV[0]));
+my $configfile;
+my $encoded;
+
+unless ($ARGV[0]) {
+    die "Must at least supply a report in the first parameter. Aborting.\n";
+}   
+
+=pod
+
+=head1 PARAMETERS
+
+ping_logger.pl requires at least one parameter - the base64 encoded report.
+
+If two parameters are given, they will be checked in order to see if they 
+match a filename on the system. If either does, it will be treated as the
+configuration file and the other will be treated as the encoded report. If
+neither matches a filename on the system, then the first will be treated as the
+report and the second will be silently discarded.
+
+=head1 CONFIGURATION
+
+=head2 Configuration parameter location
+
+Configuration parameters will be read from whichever of the following files 
+are found first. Once a file is found, no other sources are consulted for 
+configuration:
+    
+=over
+
+=item command line
+
+=item ~/.pinglogger.conf
+
+=item /etc/pinglogger.conf
+
+=item hard coded values
+
+=back
+
+=head2 Configuration parameters
+
+Configuration parameters only come from a configuration file. See above for
+the location. Within the file, lines are read one by one in the order found 
+in the file. For each line, anything from a pound sign (#) through to the end
+of the line is discarded. The line is split on the first equal sign (=) found.
+If no equal sign is found, the line is discarded.
+
+The following configuration parameter keys are defined. Any other values found 
+are discarded.
+
+=over
+
+=item datadir = The parent directory of all data written. If not specified, 
+the default value C</var/pinglogger/> is used (see CPAN File::Spec catdir
+for details of how this is handled in a cross platform way). When you supply
+the entry, it is assumed that you know how to properly supply a directory name
+locally.
+
+=back
+
+=cut
+
+$encoded = $ARGV[0];
+
+if ($ARGV[1]) {
+    # Two parameters? Check to see if either is a valid file.
+    if (-f $ARGV[0]) {
+        $encoded = $ARGV[1];
+        $configfile = $ARGV[0];
+    }
+    elsif (-f $ARGV[1]) {
+        $configfile = $ARGV[1];
+    }  
+    else {
+        # no config file specified and default of encoded in first param holds
+    }   
+}   
+else {
+    # no config file specified and default of encoded in first param holds
+}   
+
+unless ($configfile) {
+    if (-f '~/.pinglogger.conf') {
+        $configfile = '~/.pinglogger.conf';
+    }  
+    elsif (-f '/etc/pinglogger.conf') {
+        $configfile = '/etc/pinglogger.conf';
+    }   
+}
+
+$rawreport = memBunzip(decode_base64($encoded));
 
 my $twig = XML::Twig->new();
 
@@ -69,8 +159,50 @@ unless ($twig->safe_parse($rawreport)) {
         . "\n" . $@; 
 }
 
+# Configfile variables
+my $datadir;
 
-my $datadir = "/var/pinglogger/";
+if (-f $configfile) {
+    unless(open(CONF, $configfile)) {
+        die "Unable to open configuration file '$configfile', aborting\n";
+    }   
+    my $line;
+    while ($line = <CONF>) {
+        $line =~ s/\#*\Z//;
+        $line =~ s/\s\Z//;
+        if ($line =~ m/=/) {
+        	my $key;
+        	my $value;
+        	($key, $value) = split('=', $line, 2);
+        	if (lc $key eq 'datadir') {
+        	    if (-d $value) {
+        	        $datadir = $value;
+        	    }
+        	}
+        }
+        else {
+    	# No equal sign? Ignore the line
+        }
+    }
+}
+else {
+    my $tentativedir = File::Spec->catdir('var', 'pinglogger');  
+    if (-d $tentativedir) {
+        $datadir = $tentativedir;
+    }
+    else {
+        if ($datadir) {
+            die "Supplied data directory name '$datadir' is not a directory, "
+            . "aborting.\n";
+        }
+        else {
+            die "No definition of data directory found, aborting.\n";
+        }
+    }   
+}   
+
+close(CONF);
+
 
 my $formatversion = $twig->root->first_child_text('FormatVersion');
 
